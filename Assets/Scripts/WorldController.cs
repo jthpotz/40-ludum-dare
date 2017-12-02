e35eb08e56de8292b202fa5da394bea3479d8482
jthpotz <jthpotz@gmail.com>
2017-12-01 21:08:54 -0800

    made a bunch of sprites, got card movement working

---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+	private GameObject self;
+
+	// Use this for initialization
+	void Start () {
+
+		self = GameObject.FindWithTag ("WorldController");
+
+		Object.DontDestroyOnLoad (self);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
